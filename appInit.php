bbd6fcbba727973638b111926df0003d3070cd53
megrez <lujiajing1126@gmail.com>
2014-04-04 15:37:26 +0800

    start session

--- appInit.php
+++ appInit.php
@@ -2,4 +2,5 @@
 defined("APP_PATH") or define("APP_PATH",dirname(__FILE__) . '/');
 defined("APP_INC") or define("APP_INC",APP_PATH."inc/");
 defined("SUF") or define("SUF",".php");
+session_start();
 require_once(APP_INC.'auth'.SUF);
