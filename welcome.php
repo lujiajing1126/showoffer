865f87c6f236a96451ec585d80fe3ba87ca031a6
megrez <lujiajing1126@gmail.com>
2014-04-04 13:19:32 +0800

    change charset

--- welcome.php
+++ welcome.php
@@ -1,9 +1,10 @@
 <!doctype html>
 <html>
 <head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 	<title>晒你的Offer~</title>
 </head>
 <body>
-	<h4></h4>
+	<h4>Welcome To Our App</h4>
 </body>
 </html>
