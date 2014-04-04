5c1851b9201a5f4cfcccf4a226ac99786309abf0
megrez <lujiajing1126@gmail.com>
2014-04-04 21:26:10 +0800

    debug headers

--- inc/rennclient/RennClientBase.php
+++ inc/rennclient/RennClientBase.php
@@ -298,7 +298,7 @@ class RennClientBase {
 				$response = $this->http ( $url, $httpMethod, null, $headers );
 			}
 		}
-		
+		print_r($headers);
 		// // body
 		// if (isset ( $fileParams )) {
 		// // body里的参数和文件参数都用multipart方式传输
