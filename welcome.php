c839ccefc225709c17c9e4db2af44eeb97639003
megrez <lujiajing1126@gmail.com>
2014-04-04 14:16:00 +0800

    typo

--- welcome.php
+++ welcome.php
@@ -8,7 +8,7 @@
 <body>
 	<h4>Welcome To Our App</h4>
 	<button class="btn">用人人账号登陆~</button>
-	<script type="text/javascript" src+"./assets/scripts/lib/jquery/jquery.min.js"></script>
+	<script type="text/javascript" src="./assets/scripts/lib/jquery/jquery.min.js"></script>
 	<script type="text/javascript" src="./assets/scripts/lib/renren/renren.js"></script>
 	<script type="text/javascript" src="./assets/scripts/welcome.js"></script>
 </body>
