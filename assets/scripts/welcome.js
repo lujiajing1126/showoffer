5572d8d13aba65b1eadb7d07d83a6e609584b719
megrez <lujiajing1126@gmail.com>
2014-04-04 13:50:49 +0800

    add bootstrap

---
+++ assets/scripts/welcome.js
@@ -0,0 +1,17 @@
+var appInfo = {
+    APPNAME: "showoffer",
+    APPID: "266551"
+};
+var uiOpts = {
+    url: "http://graph.renren.com/oauth/authorize",
+    display: "iframe",
+    params: {
+        "response_type": "token",
+        "client_id": appInfo.APPID
+    },
+    onSuccess: function(r) {
+        top.location = "http://apps.renren.com/" + appInfo.APPNAME + "/home.php";
+    },
+    onFailure: function(r) {}
+};
+Renren.ui(uiOpts);
--- welcome.php
+++ welcome.php
@@ -3,9 +3,11 @@
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 	<title>晒你的Offer~</title>
+	<link rel="stylesheet" href="./assets/stylesheets/bootstrap.min.css">
 </head>
 <body>
 	<h4>Welcome To Our App</h4>
-	<script type="text/javascript" src="./assets/scripts/renren.js"></script>
+	<script type="text/javascript" src="./assets/scripts/lib/renren/renren.js"></script>
+	<script type="text/javascript" src="./assets/scripts/welcome.js"></script>
 </body>
 </html>
